2f3a15f1e1d006cfc767f57d7855b3ec9a9fe48c
KN <94745320+KerimNezo@users.noreply.github.com>
2022-05-22 16:05:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// bazni url za api
+const BASE_URL = "https://ptf-web-dizajn-2022.azurewebsites.net/";  
 
+// fetch koji renderuje knjige 
+fetch(`${BASE_URL}/books`)
+    .then(response => {
+        return response.json();
+    })
+    .then(books => {
+        renderBooks(books);
+    })
+
+// fetch koji renderuje autore
+fetch(`${BASE_URL}/authors`)
+    .then(response => {
+        return response.json();
+    })
+    .then(authors => {
+        renderAuthors(authors);
+    })
