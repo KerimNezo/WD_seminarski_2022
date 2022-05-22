f886f6981a0ad182fd1d12d708700a9be442fb0f
KN <94745320+KerimNezo@users.noreply.github.com>
2022-05-22 16:05:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,32 @@ const renderAuthors = (authors) => {
 
     authorRow.innerHTML = resultAuthorHtml;
 }
+
+// GET endpoint za dohvatanje podataka o knjigama koje se nalaze u bazi
+const renderBooks = (books) => {
+
+    console.log(books);
+    const booksRow = document.getElementById('books-row');
+
+    let resultHtml = '';
+
+    books.forEach(books => {
+        resultHtml += `
+        <div class="card col-4 mx-2 my-2" style="width: 15rem;">
+            <p></p>
+            <img src=${books.image} class="card-img-top img-thumbnail" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${books.name}</h5>
+                <h6>${books.author.name}</h6>
+                <p>${books.genre}</p>
+            </div>
+            <p class="sticky-bottom">
+                <button type="button" class="btn btn-warning" onclick="putBookForm('${books.id}','${books.name}','${books.genre}','${books.image}','${books.author.id}')" data-bs-toggle="modal" data-bs-target="#exampleModal2" data-bs-whatever="@getbootstrap">Edit book</button>
+                <button type="button" class="btn btn-danger" onclick="deleteBook('${books.id}')">Delete</button>
+            </p>
+        </div>
+        `;
+    });
+
+    booksRow.innerHTML = resultHtml;
+}
