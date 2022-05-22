00d314a37307d3014926018400c01e155428a2d5
KN <94745320+KerimNezo@users.noreply.github.com>
2022-05-22 16:05:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ fetch(`${BASE_URL}/authors`)
     .then(authors => {
         renderAuthors(authors);
     })
+
+// GET endpoint za dohvatanje podataka o piscima koji se nalaze u bazi
+const renderAuthors = (authors) => {
+
+    console.log(authors);
+    const authorRow = document.getElementById('author-row');
+
+    let resultAuthorHtml = '';
+
+    authors.forEach(authors => {
+        resultAuthorHtml += `
+        <div class="card col-4 mx-2 my-2" style="width: 90%">
+            <div class="card-body">
+                <h5 class="card-title">${authors.name}</h5>
+                <h6>${authors.id}</h6>
+            </div>
+        </div>
+        `;
+    });
+
+    authorRow.innerHTML = resultAuthorHtml;
+}
